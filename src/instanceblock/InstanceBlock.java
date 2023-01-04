eb98a43dd6e01f6b8d9190df0a8c4ea5702615f6
snehal kalynakar <snehal.kalyankar@vinsys.com>
2023-01-04 11:31:39 +0530

    Instance block

--- src/instanceblock/InstanceBlock.java
+++ src/instanceblock/InstanceBlock.java
@@ -1,10 +1,21 @@
 package instanceblock;
 
 public class InstanceBlock {
+	//Instance block is block which we can write the logic of the application
+	//Both instance block and constructor executed at the time of object creation
+	//But priority goes to instance block first and then constructor
+	
+	InstanceBlock(){
+		System.out.println("We are in constructor");
+	}
+	{
+		System.out.println("We are in instance block");
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		InstanceBlock iib = new InstanceBlock();
+		
 	}
 
 }
