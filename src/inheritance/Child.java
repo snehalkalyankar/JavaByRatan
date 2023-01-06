20089ca3d2752ceff6b82d868cc419fd41b1a0c6
snehal kalynakar <snehal.kalyankar@vinsys.com>
2023-01-06 18:17:15 +0530

    update

--- src/inheritance/Child.java
+++ src/inheritance/Child.java
@@ -3,14 +3,17 @@ package inheritance;
 public class Child extends Parent {
 	//if we trying to inherit constructor into child class it will you and error 
 	//add return type as void 
-	// It means that parent class constructor is consider as method in child class
+	//It means that parent class constructor is consider as method in child class
 	
 	void Parent(){
-		System.out.println("");
+		System.out.println("nanu");
 	}
 
 	public static void main(String[] args) {
-		
+		Child p = new Child();
+		p.Parent();
+		Child c=new Child();
 	}
+	
 
 }
