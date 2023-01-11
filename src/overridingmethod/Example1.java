906fbd1d81c3b399e4ad388eebc7485c525df56c
snehal kalynakar <snehal.kalyankar@vinsys.com>
2023-01-11 12:15:48 +0530

    final class

---
+++ src/overridingmethod/Example1.java
@@ -0,0 +1,8 @@
+package overridingmethod;
+
+final class Example1 {
+
+	void marry() {
+		System.out.println("Hello marry girl");
+	}
+}
---
+++ src/overridingmethod/Example11.java
@@ -0,0 +1,16 @@
+package overridingmethod;
+
+public class Example11 
+//extends Example1
+{
+
+	void marry() {
+		System.out.println("Hello marry girl1");
+	}
+	public static void main(String[] args) {
+		Example1 e = new Example1();
+		e.marry();
+
+	}
+
+}
