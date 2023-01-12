b696dfa731fcd693403325a08512131134b83cb7
snehal kalynakar <snehal.kalyankar@vinsys.com>
2023-01-12 11:22:28 +0530

    constructor in abstract class

---
+++ src/abstarction/AbstractionConstructor.java
@@ -0,0 +1,14 @@
+package abstarction;
+
+//we can call the constructor in abstarct class
+abstract class AbstractionConstructor {
+	
+	AbstractionConstructor(){
+		System.out.println("defualt constructor");
+	}
+	
+	
+
+
+
+}
---
+++ src/abstarction/AbstractionConstructor1.java
@@ -0,0 +1,11 @@
+package abstarction;
+
+public class AbstractionConstructor1 extends AbstractionConstructor {
+	
+
+	public static void main(String[] args) {
+		AbstractionConstructor1 a = new AbstractionConstructor1();
+
+	}
+
+}
